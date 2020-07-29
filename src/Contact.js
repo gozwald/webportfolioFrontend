edaa83bf9aaf830469aa925f559689cb7c7848b6
Samuel Greenwald <gozwald@gmail.com>
2020-07-29 11:41:20 +0200

    try here

--- src/Contact.js
+++ src/Contact.js
@@ -59,7 +59,7 @@ const Contact = () => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        mode: "cors",
+        mode: "no-cors",
       },
       body: JSON.stringify({
         name: e.target.name.value,
