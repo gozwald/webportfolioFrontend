2f8084f8b84b4301a2614a743bddf477b322eba7
Samuel Greenwald <gozwald@gmail.com>
2020-07-29 22:22:58 +0200

    final for today

--- src/Back.js
+++ src/Back.js
@@ -7,9 +7,12 @@ function Back() {
       style={{
         backgroundColor: "#2c2e43",
         position: "absolute",
-        // width: "100vw",
-        // height: "100vh",
+        width: "100vw",
+        height: "100vh",
         top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
         zIndex: "-1",
       }}
       params={{
