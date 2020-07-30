acde5421a44762b3659bd9912f70141f390b8397
Samuel Greenwald <gozwald@gmail.com>
2020-07-30 10:19:28 +0200

    new exp

--- src/Back.js
+++ src/Back.js
@@ -4,11 +4,11 @@ import Particles from "react-particles-js";
 function Back() {
   return (
     <Particles
+      width={"100vw"}
+      height={"100vh"}
       style={{
         backgroundColor: "#2c2e43",
         position: "absolute",
-        width: "100vw",
-        height: "100vh",
         top: 0,
         left: 0,
         right: 0,
